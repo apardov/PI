97a8f8947a8bcac268f0f076119fa571dc305344
apardov <and.pardo.vergara@gmail.com>
2023-07-18 01:19:34 -0400

    update pokemon create 18072023

--- PI-Pokemon-main/client/src/views/Form/Form.jsx
+++ PI-Pokemon-main/client/src/views/Form/Form.jsx
@@ -1,13 +1,10 @@
-//importaciones varias
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Form.module.css";
 
-// Componente Form
 const Form = () => {
-
-  const [pokemonForm, setPokemonForm] = useState({ // estado inicial del formulario
+  const [pokemonForm, setPokemonForm] = useState({
     name: "",
     img: "",
     hp: "",
@@ -19,7 +16,7 @@ const Form = () => {
     types: [],
   });
 
-  const [errors, setErrors] = useState({ // estado inicial de los errores
+  const [errors, setErrors] = useState({
     name: "",
     img: "",
     hp: "",
@@ -56,112 +53,113 @@ const Form = () => {
     });
   };
 
-  const [typeOptions, setTypeOptions] = useState([]); // estado inicial de types de pokemon
-  const [submitEnabled, setSubmitEnabled] = useState(false); // estado inicial del botón de submit
-  const [success, setSuccess] = useState(false); // estado inicial de la creación exitosa de pokemon
+  const [typeOptions, setTypeOptions] = useState([]);
+  const [submitEnabled, setSubmitEnabled] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  useEffect(() => { // hook de react para ejecutar código cuando se monta el componente
-    axios.get("http://localhost:3001/pokemons/types")   // petición get a la ruta /pokemons/types para obtener todos los types de pokemon
+  useEffect(() => {
+    axios.get("http://localhost:3001/pokemons/types")
       .then((response) => {
-        setTypeOptions(response.data.map((type) => type.name)); // guardamos los types de pokemon en el estado typeOptions
+        setTypeOptions(response.data.map((type) => type.name));
       })
       .catch(() => {
         alert("Error retrieving types");
       });
-  }, []); 
+  }, []);
 
-  useEffect(() => { // hook de react para ejecutar código cuando se actualiza el estado pokemonForm
-    validate(pokemonForm); // validamos el formulario cada vez que se actualiza el estado pokemonForm
+  useEffect(() => {
+    validate(pokemonForm);
   }, [pokemonForm]);
 
-  useEffect(() => { // hook de react para ejecutar código cuando se actualiza el estado errors
-    const hasErrors = Object.values(errors).some((error) => error !== ""); // validamos si hay errores
-    setSubmitEnabled(!hasErrors && Object.values(pokemonForm).every(value => value !== '')); // habilitamos el botón de submit si no hay errores y todos los campos del formulario están completos
+  useEffect(() => {
+    const hasErrors = Object.values(errors).some((error) => error !== "");
+    setSubmitEnabled(!hasErrors && Object.values(pokemonForm).every(value => value !== ''));
   }, [errors, pokemonForm]);
 
-  const handleChange = (e) => { // función para manejar los cambios en el formulario
-    setPokemonForm({ ...pokemonForm, [e.target.name]: e.target.value }); // actualizamos el estado pokemonForm con los valores ingresados en el formulario
+  const handleChange = (e) => {
+    setPokemonForm({ ...pokemonForm, [e.target.name]: e.target.value });
   };
 
-  const validate = (pokemonForm) => { // función para validar el formulario
-    const compiledErrors = {};  // objeto para guardar los errores
+  const validate = (pokemonForm) => {
+    const compiledErrors = {};
 
-    if (pokemonForm.name && !/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,15}$/.test(pokemonForm.name)) { // validamos el nombre del pokemon
+    if (pokemonForm.name && !/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,15}$/.test(pokemonForm.name)) {
       compiledErrors.name = "Please enter a valid name using only letters from A to Z (min 3 characters, max 15 characters)";
-    }else {
+    } else {
       compiledErrors.name = "";
     }
 
-    if (pokemonForm.img && !/\bhttps?:\/\/\S+\.(png|jpe?g)\b/.test(pokemonForm.img)) {  // validamos la url de la imagen del pokemon
+    if (pokemonForm.img && !/\bhttps?:\/\/\S+\.(png|jpe?g)\b/.test(pokemonForm.img)) {
       compiledErrors.img = "URL format is not valid";
     } else {
       compiledErrors.img = "";
     }
 
-    if (pokemonForm.hp && !/^[0-9]{1,3}$/.test(pokemonForm.hp)) { // validamos los stats del pokemon
+    if (pokemonForm.hp && !/^[0-9]{1,3}$/.test(pokemonForm.hp)) {
       compiledErrors.hp = "Please enter a valid number (Max 3 digits)";
-    }else {
+    } else {
       compiledErrors.hp = "";
     }
 
-    if (pokemonForm.attack && !/^[0-9]{1,3}$/.test(pokemonForm.attack)) { // validamos el campo attack del pokemon
+    if (pokemonForm.attack && !/^[0-9]{1,3}$/.test(pokemonForm.attack)) {
       compiledErrors.attack = "Please enter a valid number (Max 3 digits)";
-    }else {
+    } else {
       compiledErrors.attack = "";
     }
 
-    if (pokemonForm.defense && !/^[0-9]{1,3}$/.test(pokemonForm.defense)) { // validamos el campo defense del pokemon
+    if (pokemonForm.defense && !/^[0-9]{1,3}$/.test(pokemonForm.defense)) {
       compiledErrors.defense = "Please enter a valid number (Max 3 digits)";
-    }else {
+    } else {
       compiledErrors.defense = "";
     }
 
-    if (pokemonForm.speed && !/^[0-9]{1,3}$/.test(pokemonForm.speed)) { // validamos el campo speed del pokemon
+    if (pokemonForm.speed && !/^[0-9]{1,3}$/.test(pokemonForm.speed)) {
       compiledErrors.speed = "Please enter a valid number (Max 3 digits)";
-    } else{
+    } else {
       compiledErrors.speed = "";
-    } 
+    }
 
-    if (pokemonForm.height && !/^(?:[0-9]{1,3}|[0-4]\d{2}|500)(?:\.\d{1,2})?$/.test(pokemonForm.height)) { // validamos el campo height del pokemon
+    if (pokemonForm.height && !/^(?:[0-9]{1,3}|[0-4]\d{2}|500)(?:\.\d{1,2})?$/.test(pokemonForm.height)) {
       compiledErrors.height = "Please enter a valid number";
     } else {
       compiledErrors.height = "";
     }
 
-    if (pokemonForm.weight && !/^(?:[0-9]{1,3}|[0-4]\d{2}|500)(?:\.\d{1,2})?$/.test(pokemonForm.weight)) { // validamos el campo weight del pokemon
+    if (pokemonForm.weight && !/^(?:[0-9]{1,3}|[0-4]\d{2}|500)(?:\.\d{1,2})?$/.test(pokemonForm.weight)) {
       compiledErrors.weight = "Please enter a valid number";
     } else {
       compiledErrors.weight = "";
     }
-    if (pokemonForm.types.length === 0) { // validamos que se haya seleccionado al menos un type de pokemon
+
+    if (pokemonForm.types.length === 0) {
       compiledErrors.types = "Please select at least one type";
     } else {
       compiledErrors.types = "";
     }
 
-    setErrors(compiledErrors); // actualizamos el estado errors con los errores encontrados
+    setErrors(compiledErrors);
   };
 
-  const handleTypeChange = (e) => { // función para manejar los cambios en los types de pokemon
-    const { name, checked } = e.target; // destructuramos el name y el checked del target del evento
-    let selectedOptions = [...pokemonForm.types]; // creamos una copia del array de types de pokemon
-  
-    if (checked) { // si el type de pokemon está seleccionado lo agregamos al array de types de pokemon
-      selectedOptions.push(name); 
+  const handleTypeChange = (e) => {
+    const { name, checked } = e.target;
+    let selectedOptions = [...pokemonForm.types];
+
+    if (checked) {
+      selectedOptions.push(name);
     } else {
-      selectedOptions = selectedOptions.filter((option) => option !== name); // si el type de pokemon no está seleccionado lo eliminamos del array de types de pokemon
+      selectedOptions = selectedOptions.filter((option) => option !== name);
     }
-  
-    setPokemonForm({ // actualizamos el estado pokemonForm con los types de pokemon seleccionados
-      ...pokemonForm, 
+
+    setPokemonForm({
+      ...pokemonForm,
       types: selectedOptions,
     });
   };
 
-  const handleSubmit = (e) => { // función para manejar el submit del formulario
-    e.preventDefault(); // prevenimos el comportamiento por defecto del submit
-    if (submitEnabled && !errors.types) { // validamos que el botón de submit esté habilitado y que no haya errores en el campo types
-      axios.post("http://localhost:3001/pokemons", pokemonForm) // hacemos un post a la ruta /pokemons con los datos del pokemon a crear
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (submitEnabled && !errors.types) {
+      axios.post("http://localhost:3001/pokemons", pokemonForm)
         .then(() => {
           setSuccess(true);
           resetForm();
@@ -174,14 +172,11 @@ const Form = () => {
 
   return (
     <div>
-      {/* mostramos el componente Navbar */}
       <Navbar />
+      <h1>Create a Pokemon</h1>
       <div className={styles.FormContainer}>
-        <h1>Create a Pokemon</h1>
-        {/* componente en el cual ingresaremos los datos del pokemon a crear */}
         <form onSubmit={handleSubmit}>
-          <label>Name</label>
-          {/* ingresamos el nombre del pokemon */}
+          <label className={styles.label}>Name</label>
           <input
             type="text"
             name="name"
@@ -189,11 +184,9 @@ const Form = () => {
             placeholder="Pokemon Name"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.name && <span className={styles.error}>{errors.name}</span>}
 
-          <label>Image</label>
-          {/* ingresamos la url de la imagen del pokemon */}
+          <label className={styles.label}>Image</label>
           <input
             type="text"
             name="img"
@@ -201,11 +194,9 @@ const Form = () => {
             placeholder="Pokemon URL Image"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.img && <span className={styles.error}>{errors.img}</span>}
 
-          <label>HP</label>
-          {/* ingresamos los hp del pokemon */}
+          <label className={styles.label}>HP</label>
           <input
             type="text"
             name="hp"
@@ -213,11 +204,9 @@ const Form = () => {
             placeholder="Pokemon HP"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.hp && <span className={styles.error}>{errors.hp}</span>}
 
-          <label>Attack</label>
-          {/* ingresamos el attack del pokemon */}
+          <label className={styles.label}>Attack</label>
           <input
             type="text"
             name="attack"
@@ -225,11 +214,9 @@ const Form = () => {
             placeholder="Pokemon Attack"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.attack && <span className={styles.error}>{errors.attack}</span>}
 
-          <label>Defense</label>
-          {/* ingresamos el defense del pokemon */}
+          <label className={styles.label}>Defense</label>
           <input
             type="text"
             name="defense"
@@ -237,11 +224,9 @@ const Form = () => {
             placeholder="Pokemon Defense"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.defense && <span className={styles.error}>{errors.defense}</span>}
 
-          <label>Speed</label>
-          {/* ingresamos el speed del pokemon */}
+          <label className={styles.label}>Speed</label>
           <input
             type="text"
             name="speed"
@@ -249,11 +234,9 @@ const Form = () => {
             placeholder="Pokemon Speed"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.speed && <span className={styles.error}>{errors.speed}</span>}
 
-          <label>Height</label>
-          {/* ingresamos el height del pokemon */}
+          <label className={styles.label}>Height</label>
           <input
             type="text"
             name="height"
@@ -261,11 +244,9 @@ const Form = () => {
             placeholder="Pokemon Height"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.height && <span className={styles.error}>{errors.height}</span>}
 
-          <label>Weight</label>
-          {/* ingresamos el weight del pokemon */}
+          <label className={styles.label}>Weight</label>
           <input
             type="text"
             name="weight"
@@ -273,15 +254,12 @@ const Form = () => {
             placeholder="Pokemon Weight"
             onChange={handleChange}
           />
-          {/* mostramos el error en caso de que exista */}
           {errors.weight && <span className={styles.error}>{errors.weight}</span>}
 
-          <label>Pokemon Types</label>
-          {/* mostramos los types de pokemon disponibles */}
-          <div className={styles.checkboxContainer}>
+          <label className={styles.label}>Pokemon Types</label>
+          <div className={styles.typesContainer}>
             {typeOptions.map((type) => (
-              <label key={type} className={styles.checkboxLabel}>
-                {/* mostramos un checkbox por cada type de pokemon */}
+              <label key={type} className={styles.types}>
                 <input
                   type="checkbox"
                   name={type}
@@ -293,22 +271,19 @@ const Form = () => {
             ))}
           </div>
 
-          {/* validamos si el boton submit esta habilitado, en caso de ser asi se visualizara */}
           {submitEnabled ? (
-            <button type="submit">Create Pokemon</button>
+            <button type="submit" className={styles.submitButton}>
+              Create Pokemon
+            </button>
           ) : (
-            <span>Please fill in all required fields correctly</span>
+            <span className={styles.error}>Please fill in all required fields correctly</span>
           )}
-          {/* mostramos el error en caso de que exista */}
+
+          {success && <span className={styles.success}>Pokemon created successfully!</span>}
         </form>
-      </div>
-      <div>
-        {/* mostramos el mensaje de exito en caso de que se cree el pokemon */}
-        { success && <span className={styles.success}>Pokemon created successfully!</span> }
       </div>
     </div>
   );
 }
 
 export default Form;
-
